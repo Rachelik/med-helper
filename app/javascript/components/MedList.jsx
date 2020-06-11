be39e0ca2941cc63bd1c765a0ed3ed8df971f651
Rachelik <rachel.ik.87@gmail.com>
2020-06-11 17:01:42 +0800

    Able to display by required section. date not working yet.

--- app/javascript/components/MedList.jsx
+++ app/javascript/components/MedList.jsx
@@ -10,89 +10,163 @@ class MedList extends React.Component {
 
     this.state = {
         meds: [],
-        before_food: [],
-        after_food: [],
-        date_end: null
+        bf_breakfast: [],
+        aft_breakfast: [],
+        bf_lunch: [],
+        aft_lunch: [],
+        bf_dinner: [],
+        aft_dinner: []
     };
   }
-  async componentDidMount() {
-    const url = '/meds.json';
-    const runWhenDone = (response) => {
-       const dataB = response.data.filter(med => med.indication.includes('Before food'))
-       const dataA = response.data.filter(med => med.indication.includes('After food'))
-       this.setState({ before_food: dataB, after_food: dataA })
-    }
-    const whenError = (error) => {
-      console.log("error", error)
-    }
-    await axios.get(url).then(runWhenDone).catch(whenError);
-  }
 
-//when clicked, it will be able to show one of the meds only
-//want to change this to be able to edit.
-  clickedHandler=(event) =>{
-    console.log("name clicked!!!! " + event.target.innerText)
-  //   let med = this.state.meds.filter(med => med.name.includes(event.target.innerText))
-  //   let date_end = med.date_start + med.duration
-  //   console.log(moment(med[0].date_start).add(med[0].duration, 'day').format('DD MMM YYYY hh:mm'))
-  //   this.setState({
-  //     meds: med
-  //   })
+  async componentDidMount() {
+    const runWhenDone = (response) => {
+      let allData = response.data;
+
+      allData.forEach(med => {
+        let date_end = moment(med.date_start).add(med.duration, 'day')
+        if(Date.now() >= moment(med.date_start) && Date.now() < date_end){
+          //check for breakfast
+          if((med.frequency === 1 && med.time === 'Morning') || med.frequency === 2 || med.frequency === 3) {
+            if (med.indication === 'Before food') {
+              const medsBfBreakfast = [med, ...this.state.bf_breakfast];
+              this.setState({bf_breakfast: medsBfBreakfast})
+            } else if (med.indication === 'After food') {
+              const medsAftBreakfast = [med, ...this.state.aft_breakfast];
+              this.setState({aft_breakfast: medsAftBreakfast})
+            }
+          }
+          //check for dinner
+          if ((med.frequency === 1 && med.time === 'Night') || med.frequency === 2 || med.frequency === 3) {
+            if (med.indication === 'Before food') {
+              const medsBfDinner = [med, ...this.state.bf_dinner];
+              this.setState({bf_dinner: medsBfDinner})
+            } else if (med.indication === 'After food') {
+              const medsAftDinner = [med, ...this.state.aft_dinner];
+              this.setState({aft_dinner: medsAftDinner})
+            }
+          }
+          //check for lunch
+          if (med.frequency === 3) {
+            if (med.indication === 'Before food') {
+              const medsBfLunch = [med, ...this.state.bf_lunch];
+              this.setState({bf_lunch: medsBfLunch})
+            } else if (med.indication === 'After food') {
+              const medsAftLunch = [med, ...this.state.aft_lunch];
+              this.setState({aft_lunch: medsAftLunch})
+            }
+          }
+        }
+      })
+    }
+    await axios.get('/meds.json').then(runWhenDone).catch((error) => {
+      console.log("error", error)
+    });
   }
 
   render() {
-    let before_food = this.state.before_food
+    let bfBreakfast = this.state.bf_breakfast
       .map((med, index)=>{
         return (
           <div className="med" key={med.id}>
             <div className="med-info">
-             {/*<a href="#" onClick={this.clickedHandler}><h4>{med.name}</h4></a>*/}
-              <p>{med.name}, {med.dosage}</p>
-              <p>{moment(med.date_start).format()}</p>
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
             </div>
           </div>
         );
     });
 
-    let after_food = this.state.after_food
+    let aftBreakfast = this.state.aft_breakfast
       .map((med, index)=>{
         return (
           <div className="med" key={med.id}>
             <div className="med-info">
-              <p>{med.name}, {med.dosage}</p>
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
             </div>
           </div>
         );
     });
 
-    // let med = this.state.meds
-    //   .filter(med => med.name.includes(this.state.click))
-    //   .map((med, index)=>{
-    //     return (
-    //       <div className="med" key={med.id}>
-    //         <div className="med-info">
-    //           <a href="#" onClick={this.clickedHandler}><h4>{med.name}</h4></a>
-    //           <p>{med.indication}</p>
-    //           <p>{med.dosage}</p>
-    //           <p>{med.date_start}</p>
-    //           <p>{med.frequency}</p>
-    //         </div>
-    //       </div>
-    //     );
-    // });
+    let bfLunch = this.state.bf_lunch
+      .map((med, index)=>{
+        return (
+          <div className="med" key={med.id}>
+            <div className="med-info">
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
+            </div>
+          </div>
+        );
+    });
+
+    let aftLunch = this.state.aft_lunch
+      .map((med, index)=>{
+        return (
+          <div className="med" key={med.id}>
+            <div className="med-info">
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
+            </div>
+          </div>
+        );
+    });
+
+    let bfDinner = this.state.bf_dinner
+      .map((med, index)=>{
+        return (
+          <div className="med" key={med.id}>
+            <div className="med-info">
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
+            </div>
+          </div>
+        );
+    });
+
+    let aftDinner = this.state.aft_dinner
+      .map((med, index)=>{
+        return (
+          <div className="med" key={med.id}>
+            <div className="med-info">
+              <p>{med.name}, {med.dosage}, {med.indication}, {med.time}, {med.frequency}</p>
+            </div>
+          </div>
+        );
+    });
 
     return (
         <div className="container">
-          <div className="med-list">
-              {before_food}
+          <div className="section breakfast-section">
+            <div className="med-list">
+              {bfBreakfast}
+            </div>
+            <div className="section-title">
+              <h3>Breakfast</h3>
+            </div>
+            <div className="med-list">
+              {aftBreakfast}
+            </div>
           </div>
-          <div>
-            <hr/>
-              <h4>Meals</h4>
-            <hr/>
+
+          <div className="section lunch-section">
+            <div className="med-list">
+              {bfLunch}
+            </div>
+            <div className="section-title">
+              <h3>Lunch</h3>
+            </div>
+            <div className="med-list">
+              {aftLunch}
+            </div>
           </div>
-          <div className="med-list">
-              {after_food}
+
+          <div className="section dinner-section">
+            <div className="med-list">
+              {bfDinner}
+            </div>
+            <div className="section-title">
+              <h3>Dinner</h3>
+            </div>
+            <div className="med-list">
+              {aftDinner}
+            </div>
           </div>
         </div>
     );
